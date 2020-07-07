f2f6fec53307a068d489d8fb75d9caba1086af59
Christeen T Jose <christeenjosethoomkuzhy@gmail.com>
2020-07-07 16:28:47 +0530

    Add files via upload

---
+++ ROT13 - Encryption and Decryption.py
@@ -0,0 +1,13 @@
+C2I=dict(zip('ABCDEFGHIJKLMNOPQRSTUVWXYZ',range(26)))
+I2C=dict(zip(range(26),'ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+#INPUTS
+plaintext=input('Enter the plaintext to be encrypted: ').upper()
+key=13
+#ROT13 CIPHER
+ciphertext=[]
+for word in plaintext.split(' '):
+	ciphertext.append(''.join([I2C[(C2I[i]+key)%26] for i in word]))
+ciphertext=' '.join(ciphertext)
+#OUTPUTS
+print('\n\n\t\tPlaintext:  ',plaintext)
+print('\n\n\t\tCiphertext: ',ciphertext)
